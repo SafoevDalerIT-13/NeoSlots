363b65ba5da91d280a7227a4714b20ff5c8dec68
SafoevDalerIT-13 <dsafoev40@gmail.com>
2026-05-21 13:51:03 +0300

    Initial commit

---
+++ src/main/java/com/safoev/NeoSlots/NeoSlotsApplication.java
@@ -0,0 +1,13 @@
+package com.safoev.NeoSlots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoSlotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoSlotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safoev/NeoSlots/NeoSlotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safoev.NeoSlots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoSlotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
